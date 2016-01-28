f43712d7e13eab0fbcc9714e4f149159086ae884
jstevens1973 <jeff.robert.stevens@gmail.com>
2016-01-28 13:29:55 -0500

    Tryparse update

---
+++ ExperienceCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperienceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime Value1;
+            DateTime Value2;
+
+                if (DateTime.TryParse("January 2013", out Value1)){
+
+                Console.WriteLine(Value1.Month);
+            
+
+                }else{
+                    Console.WriteLine ("Error");
+                }
+            Console.ReadLine();
+        }
+    }
+}
